d4f6096fcd74f62c094541da20b7a5d9f9ff548e
assassin0210 <alexander.s@helium10.com>
2022-01-06 21:53:23 +0200

    ++ my works

--- src/common/MyWorkItem.tsx
+++ src/common/MyWorkItem.tsx
@@ -3,16 +3,24 @@ import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { FlexCenter } from "../styled/common";
 import { SocialMedia } from "./SocialMedia";
+interface IProp {
+  link: string;
+  src: string;
+  git: string;
+  description: string;
+}
 
-export const MyWorkItem = () => {
+export const MyWorkItem = ({ src, git, description, link }: IProp) => {
   return (
-    <Wrapper>
-      <HoverSection>
-        <SocialMedia $size={"50px"} icon={faGithubSquare} href="/" />
-        <SocialMedia $size={"50px"} icon={faLink} href="/" />
-        <Links />
-      </HoverSection>
-    </Wrapper>
+    <>
+      <Wrapper src={src}>
+        <HoverSection>
+          <SocialMedia $size={"60px"} icon={faGithubSquare} href={git} />
+          <SocialMedia $size={"60px"} icon={faLink} href={link} />
+          <Links />
+        </HoverSection>
+      </Wrapper>
+    </>
   );
 };
 
@@ -24,24 +32,31 @@ const HoverSection = styled(FlexCenter)`
 const Links = styled.div`
   position: absolute;
   border-radius: 8px;
-
-  width: 88%;
+  width: 100%;
   pointer-events: none;
-  height: 90%;
-  background-color: rgba(255, 255, 255, 0.1);
+  height: 100%;
+  z-index: -1;
+  opacity: 0.5;
+  background-color: rgb(45%, 46%, 47%);
 `;
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ src: string }>`
+  cursor: pointer;
   border-radius: 8px;
   position: relative;
   width: 230px;
   height: 300px;
-  background: #000;
+  background: ${({ src }) => `url(${src})`} no-repeat center;
+  background-size: 110%;
   display: flex;
   align-items: center;
   justify-content: center;
   overflow: hidden;
+  transition: all 0.3s ease-in-out;
+
   :hover {
+    transform: scale(1.1);
+
     ${HoverSection} {
       opacity: 1;
     }
--- src/components/MyWorks.tsx
+++ src/components/MyWorks.tsx
@@ -7,21 +7,82 @@ import {
 import { MyWorkItem } from "../common/MyWorkItem";
 import styled from "styled-components";
 import { useTranslate } from "../hooks/translate";
+import waxon from "../static/works/waxon.jpg";
+import tesla from "../static/works/tesla.jpg";
+import music from "../static/works/music.jpg";
+import pizza from "../static/works/pizza.jpg";
+import basketboll from "../static/works/basketboll.jpg";
+import summary from "../static/works/summary.jpg";
+import game from "../static/works/game.jpg";
+import registration from "../static/works/registration.jpg";
 
 export const MyWorks = () => {
   const { t } = useTranslate();
+
+  const data = [
+    {
+      link: "/",
+      git: "https://github.com/assassin0210/waxon",
+      src: waxon,
+      description: "Waxon Landing",
+    },
+    {
+      link: "/",
+      git: "https://github.com/assassin0210/teslaProject",
+      src: tesla,
+      description: "Tesla Test Drive",
+    },
+    {
+      link: "/",
+      git: "https://github.com/assassin0210/music",
+      src: music,
+      description: "Music Player",
+    },
+    {
+      link: "/",
+      git: "https://github.com/assassin0210/pizzza",
+      src: pizza,
+      description: "pizza",
+    },
+    {
+      link: "/",
+      git: "https://github.com/assassin0210/basketball",
+      src: basketboll,
+      description: "basketboll",
+    },
+    {
+      link: "/",
+      git: "https://github.com/assassin0210/summary",
+      src: summary,
+      description: "summary",
+    },
+    {
+      link: "/",
+      git: "https://github.com/assassin0210/table-info",
+      src: game,
+      description: "game",
+    },
+    {
+      link: "/",
+      git: "https://github.com/assassin0210/registration",
+      src: registration,
+      description: "registration",
+    },
+  ];
   return (
     <Container>
       <ScrollSection>
         <Title>{t.myWorks.title}</Title>
         <TextWithLine>123</TextWithLine>
         <Wrapper>
-          <MyWorkItem />
-          <MyWorkItem />
-          <MyWorkItem />
-          <MyWorkItem />
-          <MyWorkItem />
-          <MyWorkItem />
+          {data.map((item) => (
+            <MyWorkItem
+              src={item.src}
+              link={item.link}
+              git={item.git}
+              description={item.description}
+            />
+          ))}
         </Wrapper>
       </ScrollSection>
     </Container>
