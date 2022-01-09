16a38c11dd177ffd16b509ea08faa12f6bacd5f8
assassin0210 <alexander.s@helium10.com>
2022-01-09 14:35:56 +0200

    delete pin on map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,6 @@
-import React, { FC } from "react";
+import React from "react";
 import GoogleMapReact from "google-map-react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
-import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 const location = {
   address: "Tiraspol 3300, Moldova",
@@ -15,29 +13,9 @@ export const Map = () => (
       bootstrapURLKeys={{ key: "AIzaSyDrEWOIhhdDXpaOMaPVsHqnf7AOreDZ0OY" }}
       defaultCenter={location}
       defaultZoom={12}
-    >
-      <LocationPin text={location.address} />
-    </GoogleMapReact>
+    />
   </Wrapper>
 );
-
-const LocationPin: FC<{ text: string }> = ({ text }) => {
-  return (
-    <Pin>
-      <PinIcon />
-      <p>{text}</p>
-    </Pin>
-  );
-};
-const PinIcon = styled(FontAwesomeIcon).attrs({ icon: faMapMarkerAlt })`
-  color: ${({ theme }) => theme.color.red};
-  font-size: 38px;
-`;
-
-const Pin = styled.div`
-  display: flex;
-  gap: 10px;
-`;
 
 const Wrapper = styled.div`
   display: flex;
